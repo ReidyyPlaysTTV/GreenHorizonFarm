a0e716146b522a1acc3a00fc6311a1b2cbd69d5c
Liam Reid <PROxLee97@gmail.com>
2025-08-27 21:22:23 +0000

    Dont have a Character Limit on Passwords

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -23,9 +24,7 @@ const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
-  password: z.string().min(8, {
-    message: "Password must be at least 8 characters.",
-  }),
+  password: z.string(),
 });
 
 export function LoginForm() {
